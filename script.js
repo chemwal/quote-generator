c4e6c98198254f1aff1d3c501622fdf722d8aed8
Ujjwal <manitripathiujjwal@gmail.com>
2021-09-30 16:40:28 +0530

    Added Basic JS using API FETCH

---
+++ script.js
@@ -0,0 +1,33 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  //Pick a random quote
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes from API
+
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here.
+  }
+}
+
+//On Load
+getQuotes();
